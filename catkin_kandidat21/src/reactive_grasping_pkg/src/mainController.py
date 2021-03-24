8928b2d15ac70b86d9df0b961f910a3bdd56b6f8
Henrik <anderssh@student.chalmers.se>
2021-03-24 22:27:02 +0100

    added comments

--- catkin_kandidat21/src/reactive_grasping_pkg/src/mainController.py
+++ catkin_kandidat21/src/reactive_grasping_pkg/src/mainController.py
@@ -4,12 +4,15 @@ import numpy
 
 class MainController:
 
+    """constructor with printmessage for debugging"""
     def __init__(self):
-        print('MainController constructor called')
+        print('MainController constructor is called')
 
+    """method converts degree to radian"""
     def degree_to_radian(self, degree):
         return numpy.deg2rad(degree)
 
+    """method convert radian to degree"""
     def radian_to_degree(self, radian):
         return numpy.rad2deg(radian)
 
@@ -17,7 +20,7 @@ class MainController:
         None
 
 
-#simple testing
+"""simple testing of methods in MainController"""
 mc1 = MainController()
 print('mc1.degree_to_radian()', mc1.degree_to_radian(10))
 print('mc2.radian_to_degree()', mc1.radian_to_degree(1))
