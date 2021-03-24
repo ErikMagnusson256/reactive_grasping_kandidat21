7b163da91f9a6a02259bdf7e8fd189d9692b9ec0
Henrik <anderssh@student.chalmers.se>
2021-03-24 22:17:00 +0100

    rad_to_degree and degree_to_rad practicing

--- catkin_kandidat21/src/reactive_grasping_pkg/src/mainController.py
+++ catkin_kandidat21/src/reactive_grasping_pkg/src/mainController.py
@@ -1,4 +1,25 @@
-# TODO
+#! /usr/bin/env python
+import numpy
 
-if __name__ == '__main__':
-    None
+
+class MainController:
+
+    def __init__(self):
+        print('MainController constructor called')
+
+    def degree_to_radian(self, degree):
+        return numpy.deg2rad(degree)
+
+    def radian_to_degree(self, radian):
+        return numpy.rad2deg(radian)
+
+    if __name__ == '__main__':
+        None
+
+
+#simple testing
+mc1 = MainController()
+print('mc1.degree_to_radian()', mc1.degree_to_radian(10))
+print('mc2.radian_to_degree()', mc1.radian_to_degree(1))
+
+
